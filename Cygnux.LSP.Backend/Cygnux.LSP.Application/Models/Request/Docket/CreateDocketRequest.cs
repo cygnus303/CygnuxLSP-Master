2a0cb49cb1b032477611c41d6688ee52149aa112
Virisha Patel <virisha.cygnux92@gmail.com>
2025-03-31 17:20:41 +0530

    UserId change to EntryBy

--- Cygnux.LSP.Backend/Cygnux.LSP.Application/Models/Request/Docket/CreateDocketRequest.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Application/Models/Request/Docket/CreateDocketRequest.cs
@@ -14,7 +14,7 @@ namespace Cygnux.LSP.Application.Models.Request.Docket
         public string Transporter { get; set; } = string.Empty;
         public string TransportMode { get; set; } = string.Empty;
         public int Quantity { get; set;}
-        public Guid UserId  { get; set; } 
+        public Guid EntryBy  { get; set; } 
         public bool IsCancel { get; set; }
     }
 }
