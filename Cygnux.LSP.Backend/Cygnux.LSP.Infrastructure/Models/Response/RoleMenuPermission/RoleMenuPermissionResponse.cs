fa576b9d6be042239bc7202e54d1240cc7e42bac
Virisha Patel <virisha.cygnux92@gmail.com>
2025-04-01 16:15:24 +0530

    Added CanCreate property in class file

--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/RoleMenuPermission/RoleMenuPermissionResponse.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/RoleMenuPermission/RoleMenuPermissionResponse.cs
@@ -5,6 +5,7 @@ public class RoleMenuPermissionResponse
     public Guid? RoleId { get; set; }
     public Guid MenuId { get; set; }
     public string MenuName { get; set; } = string.Empty;
+    public bool CanCreate { get; set; }
     public bool CanView { get; set; }
     public bool CanEdit { get; set; }
     public bool CanDelete { get; set; }
