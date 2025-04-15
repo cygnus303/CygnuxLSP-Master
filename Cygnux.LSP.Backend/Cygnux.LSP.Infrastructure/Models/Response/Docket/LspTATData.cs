a91fdbae7678d73f8db4d989bd1da6c654410e80
Virisha Patel <virisha.cygnux92@gmail.com>
2025-04-15 11:11:14 +0530

    Validate and From and To LSP tat Commit

---
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Docket/LspTATData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cygnux.LSP.Infrastructure.Models.Response.Docket
+{
+    public class LspTATData
+    {
+        public string? Location { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid LspId {  get; set; }
+    }
+}
