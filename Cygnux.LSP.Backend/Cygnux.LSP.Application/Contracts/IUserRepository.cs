6c506dbfbcfc155d08134b689db79fa133bc02b0
Gopi <gopi@cygnux.in>
2025-04-25 12:32:44 +0530

    add userId

--- Cygnux.LSP.Backend/Cygnux.LSP.Application/Contracts/IUserRepository.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Application/Contracts/IUserRepository.cs
@@ -7,7 +7,7 @@ using Models.Response;
 
 public interface IUserRepository
 {
-    Task<BaseResponse<IEnumerable<UserResponse>>> GetUserList(int page, int pageSize);
+    Task<BaseResponse<IEnumerable<UserResponse>>> GetUserList(int page, int pageSize,Guid usreId);
 
     Task<BaseResponse<UserResponse?>> GetUserDetails(Guid id);
 
