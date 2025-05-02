426a21d72c191b4fbea5c3fbcada91203c383f71
Virisha Patel <virisha.cygnux92@gmail.com>
2025-05-02 17:23:33 +0530

    added 2 fields in docket list response

--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Docket/DocketListResponse.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Docket/DocketListResponse.cs
@@ -16,6 +16,8 @@
         public DateTime EntryDate { get; set; }
         public int? TotalCount { get; set; }
         public TrackingList track {  get; set; }
+        public string TransporterDesc { get; set; } = string.Empty;
+        public string TransportModeDesc { get; set; } = string.Empty;
     }
 
     public class TrackingList
