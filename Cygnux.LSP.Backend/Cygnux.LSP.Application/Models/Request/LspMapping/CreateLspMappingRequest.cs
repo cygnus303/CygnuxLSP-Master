38414ef16c9eb24106a9b95cdb8c8d212839366f
Virisha Patel <virisha.cygnux92@gmail.com>
2025-05-05 17:25:04 +0530

    LSp details change

--- Cygnux.LSP.Backend/Cygnux.LSP.Application/Models/Request/LspMapping/CreateLspMappingRequest.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Application/Models/Request/LspMapping/CreateLspMappingRequest.cs
@@ -5,13 +5,10 @@ using Infrastructure.Implementations;
 public class CreateLspMappingRequest : UserSettings
 {
     public Guid CustomerId { get; set; }
-    /*public string[] LspIds { get; set; }*/
-    /*public required Guid LspId { get; set; }*/
     public required string LspId { get; set; }
     public bool? IsActive { get; set; }
     public string SupportEmail { get; set; } = string.Empty;
-
-    /*public Guid? LspMapId { get; set; }*/
+    public Guid? LspMappingId { get; set; } 
 }
 
 
