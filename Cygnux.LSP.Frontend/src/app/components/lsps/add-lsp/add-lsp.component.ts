cc9c201a8176c1ecabb2176e6c9d0a8baa580f7b
Archi <archivirani01@gmail.com>
2025-05-29 18:02:00 +0530

    remove debugger

--- Cygnux.LSP.Frontend/src/app/components/lsps/add-lsp/add-lsp.component.ts
+++ Cygnux.LSP.Frontend/src/app/components/lsps/add-lsp/add-lsp.component.ts
@@ -106,7 +106,6 @@ export class AddLspComponent implements OnInit, OnChanges {
         this.selectedFile = file;
         this.fileError = null;
         this.selectedFileName = file.name
-        debugger
         this.lspForm.get('logo')?.setValue('https://uatlspapi.cygnux.in/Uploads/'+file.name);
 
         this.lspForm.get('file')?.markAsTouched();
