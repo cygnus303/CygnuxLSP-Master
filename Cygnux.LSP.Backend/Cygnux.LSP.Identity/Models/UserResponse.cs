967b26f8618e9b945934559575d9f0854a74eae7
Virisha Patel <virisha.cygnux92@gmail.com>
2025-05-30 15:51:03 +0530

    User roles

--- Cygnux.LSP.Backend/Cygnux.LSP.Identity/Models/UserResponse.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Identity/Models/UserResponse.cs
@@ -5,12 +5,10 @@ public class UserResponse
     public Guid Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-
     public string? EmailId { get; set; }
     public bool IsActive { get; set; }
     public string? PhoneNumber { get; set; }
-
-    public string Roles { get; set; }
+    public string? Roles { get; set; }
     public int? TotalCount { get; set; }
     public string? City { get; set; } = string.Empty;
     public string? CustomerName { get; set; } = string.Empty;
