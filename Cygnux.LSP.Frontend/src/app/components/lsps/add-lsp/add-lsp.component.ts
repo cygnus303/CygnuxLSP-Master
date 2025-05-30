b42904129b275f9ac694ed30479bcfeef57e00d9
Archi <archivirani01@gmail.com>
2025-05-30 17:29:09 +0530

    remove sattic url

--- Cygnux.LSP.Frontend/src/app/components/lsps/add-lsp/add-lsp.component.ts
+++ Cygnux.LSP.Frontend/src/app/components/lsps/add-lsp/add-lsp.component.ts
@@ -105,7 +105,7 @@ export class AddLspComponent implements OnInit, OnChanges {
         this.selectedFile = file;
         this.fileError = null;
         this.selectedFileName = file.name
-        this.lspForm.get('logo')?.setValue('https://uatlspapi.cygnux.in/Uploads/'+file.name);
+        this.lspForm.get('logo')?.setValue(file.name);
 
         this.lspForm.get('file')?.markAsTouched();
   
