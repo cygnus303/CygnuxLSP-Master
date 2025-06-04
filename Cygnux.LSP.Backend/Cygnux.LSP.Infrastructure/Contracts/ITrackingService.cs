d69476d43defb68e36f8fd2cc05790c2677888f9
Virisha Patel <virisha.cygnux92@gmail.com>
2025-06-04 17:57:04 +0530

    Tracking API update

--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Contracts/ITrackingService.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Contracts/ITrackingService.cs
@@ -1,10 +1,11 @@
 ﻿namespace Cygnux.LSP.Infrastructure.Contracts;
 
 using Cygnux.LSP.Infrastructure.Models.Response.Docket;
+using Cygnux.LSP.Infrastructure.Models.Response.Tracking;
 using Models.Response;
 using Models.Response.LspMapping;
 
 public interface ITrackingservice
 {
-    Task<IEnumerable<DocketListResponse>> GetTrackigList(string docketNOs, Guid userid);
+    Task<IEnumerable<TrackingDocketResponse>> GetTrackigList(string docketNOs, Guid userid);
 }
--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Implementations/TrackingService.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Implementations/TrackingService.cs
@@ -3,6 +3,7 @@
 using Constants;
 using Contracts;
 using Cygnux.LSP.Infrastructure.Models.Response.Docket;
+using Cygnux.LSP.Infrastructure.Models.Response.Tracking;
 using Dapper;
 using Models.Response;
 
@@ -17,13 +18,13 @@ internal class TrackingService : ITrackingservice
         _dbConnection = dbConnection;
     }
 
-    public async Task<IEnumerable<DocketListResponse>> GetTrackigList(string docketNOs, Guid userid)
+    public async Task<IEnumerable<TrackingDocketResponse>> GetTrackigList(string docketNOs, Guid userid)
     {
         var parameters = new DynamicParameters();
         parameters.Add("@docknumber", docketNOs, DbType.String);
         parameters.Add("@UserId", userid, DbType.Guid);
 
-        return await _dbConnection.QueryAsync<DocketListResponse>(
+        return await _dbConnection.QueryAsync<TrackingDocketResponse>(
              StoredProcedureConstants.Usp_TrackingList,
              parameters,
              commandType: CommandType.StoredProcedure
