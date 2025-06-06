5dfa653ad6287ff0510fa51d885e395d2f090ab6
Gopi <gopi@cygnux.in>
2025-06-06 17:05:41 +0530

    add pod and status update

--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/RoleMenuPermission/RoleMenuPermissionResponse.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/RoleMenuPermission/RoleMenuPermissionResponse.cs
@@ -9,4 +9,6 @@ public class RoleMenuPermissionResponse
     public bool CanView { get; set; }
     public bool CanEdit { get; set; }
     public bool CanDelete { get; set; }
+    public bool CanPOD { get; set; }
+    public bool CanStatusUpdate { get; set; }
 }
