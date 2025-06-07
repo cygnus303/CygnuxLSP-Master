e108f25ab6ea12c7ca337be3b39fe969e00db682
Gopi <gopi@cygnux.in>
2025-06-07 12:16:28 +0530

    add filed

--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Menu/MenuResponse.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Menu/MenuResponse.cs
@@ -8,6 +8,8 @@ public class MenuResponse
     public bool CanCreate { get; set; } 
     public bool CanEdit { get; set; } 
     public bool CanDelete { get; set; }
+    public bool CanPOD { get; set; }
+    public bool CanstatusUpdate { get; set; }
 
     public string NavigationUrl { get; set; } = string.Empty;
     public bool IsActive { get; set; }
