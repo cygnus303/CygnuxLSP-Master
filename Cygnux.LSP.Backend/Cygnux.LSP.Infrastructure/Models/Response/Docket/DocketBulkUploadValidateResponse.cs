8d23f089acad82d7a0f0d40cafe753d61234a3c8
virisha <virisha@cygnux.in>
2025-07-24 12:12:16 +0530

    Front and Back both images validation in POD upload

--- Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Docket/DocketBulkUploadValidateResponse.cs
+++ Cygnux.LSP.Backend/Cygnux.LSP.Infrastructure/Models/Response/Docket/DocketBulkUploadValidateResponse.cs
@@ -106,6 +106,8 @@ namespace Cygnux.LSP.Infrastructure.Models.Response.Docket
         public DateTime UploadDate { get; set; }
         public string ImageLink { get; set; } = string.Empty;
         public string ImageName { get; set; } = string.Empty;
+        public string ImageFront {  get; set; } = string.Empty;
+        public string ImageBack { get; set; } = string.Empty ;
         public Guid? LSPId { get; set; }
         public string LSPName { get; set; } = string.Empty;
         public Guid? CustomerId { get; set; }
